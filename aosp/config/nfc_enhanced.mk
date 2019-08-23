8f6bf6f2bdb8290cf72657af5b951b628348359d
Dev-light1234 <shinsooan0610@gmail.com>
2019-08-23 16:07:34 +0900

    chenging to other aosp vendor...

--- aosp/config/nfc_enhanced.mk
+++
@@ -1,4 +0,0 @@
-# This device supports CM enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
-
