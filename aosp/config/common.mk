bad430784d4905b25cef33d1f4caa4cd37526e56
Dev-light1234 <shinsooan0610@gmail.com>
2019-08-26 16:57:34 +0900

    deleted adding aex features while building

---
+++ aosp/config/common.mk
@@ -0,0 +1,158 @@
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# include definitions for SDCLANG
+include vendor/aosp/sdclang/sdclang.mk
+
+PRODUCT_BRAND ?= AOSP
+
+# Use signing keys for user builds
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp/.keys/release
+endif
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common \
+    vendor/aosp/overlay/dictionaries
+
+# Proprietary latinime libs needed for Keyboard swyping
+ifneq ($(filter arm64,$(TARGET_ARCH)),)
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/aosp/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+    vendor/aosp/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+endif
+
+# EXT4/F2FS format script
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/format.sh:install/bin/format.sh
+
+ifeq ($(DEFAULT_ROOT_METHOD),magisk)
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/zip/magisk.zip:system/addon.d/magisk.zip
+
+else ifeq ($(DEFAULT_ROOT_METHOD),supersu)
+# SuperSU
+PRODUCT_COPY_FILES += \
+   vendor/aosp/prebuilt/common/zip/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+endif
+endif
+
+# Custom AOSP packages
+PRODUCT_PACKAGES += \
+    BluetoothExt \
+    LatinIME \
+    MusicFX \
+    Launcher3 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    Stk \
+    ThemeInterfacer \
+    WallpaperPickerGoogle \
+    ViaBrowser \
+    CalendarWidget \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    Phonograph \
+    SoundRecorder \
+    Turbo
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    mkfs.ntfs \
+    fsck.ntfs \
+    mount.ntfs \
+    7z \
+    bzip2 \
+    curl \
+    lib7z \
+    powertop \
+    pigz \
+    tinymix \
+    unrar \
+    unzip \
+    zip
+
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
+
+# AOSP-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.aosp.rc
+
+# Bring in camera effects
+PRODUCT_COPY_FILES +=  \
+    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Copy over added mimetype supported in libcore.net.MimeUtils
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+    
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
+
+# Needed by some RILs and for some gApps packages
+PRODUCT_PACKAGES += \
+    librsjni \
+    libprotobuf-cpp-full
+
+# Charger images
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG ?= false
+
+# TCM (TCP Connection Management)
+PRODUCT_PACKAGES += \
+    tcmiface
+
+PRODUCT_BOOT_JARS += \
+    tcmiface
